fad49531882f3996041c1c1a0bd909bc40c39933
Rômulo Ponciano <romponciano@gmail.com>
2021-08-31 01:28:13 -0300

    add interfaces

---
+++ src/token/interfaces.d.tsx
@@ -0,0 +1,83 @@
+export { IToken }
+
+interface IToken {
+    basic: IBasicInfo
+    life: number
+    chakra: number
+    attributes: IAttributes
+    skills: ISkills
+    elements: IElements
+    advantages: ICharacteristic[]
+    disadvantages: ICharacteristic[]
+    missions: IMissions
+}
+
+interface IPlayer {
+    id: String
+    token: IToken[]
+}
+
+interface IJutsu {
+    id: String
+    name: String
+    description: String
+    cost: String
+}
+
+interface ICharacteristic {
+    id: String
+    name: String
+    description: String
+    value: number
+}
+
+interface IBasicInfo {
+    name: String
+    clan: String
+    village: String
+    level: ILevel
+    nivel: String
+}
+
+interface ILevel {
+    value: number
+    unity: String
+}
+
+interface IAttributes {
+    strenght: number
+    spirit: number
+    resistence: number
+    carism: number
+    dexterity: number
+    intelligence: number
+}
+
+interface ISkills {
+    ninjutsu: number
+    igakujutsu: number
+    genjutsu: number
+    bukijutsu: number
+    taijutsu: number
+    fuinjutsu: number
+    ichizoku: number
+    senjutsu: number
+}
+
+interface IElements {
+    water: number
+    fire: number
+    earth: number
+    thunder: number
+    wind: number
+    main: String[]
+}
+
+interface IMissions {
+    sPlus: number
+    s: number
+    a: number
+    b: number
+    c: number
+    d: number
+}
